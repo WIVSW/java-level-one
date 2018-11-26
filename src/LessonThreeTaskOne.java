3e2800cec95ee89f15b5b2e847a0bc13fe1ee3bb
WIVSW <olegspostnikov@gmail.com>
2018-11-26 23:01:04 +0500

    Define constansts

---
+++ src/LessonThreeTaskOne.java
@@ -0,0 +1,11 @@
+public class LessonThreeTaskOne {
+
+    private static int MAX_ATTEMPTS_COUNT = 3;
+
+    private static int LIMIT_RANDOM = 10;
+
+    private static int attemptsLeft;
+
+    public static void main(String[] args) {}
+
+}
