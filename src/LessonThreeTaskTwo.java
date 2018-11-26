cffd7ddd247380ac8dca0dba0f86e6ae7af05311
WIVSW <olegspostnikov@gmail.com>
2018-11-27 01:11:17 +0500

    Add class LessonThreeTaskTwo

---
+++ src/LessonThreeTaskTwo.java
@@ -0,0 +1,11 @@
+/**
+ * Java. Level 1. Lesson 3. Task 1. Practice
+ *
+ * @author Oleg Postnikov
+ * @version dated Nov 27, 2018
+ */
+public class LessonThreeTaskTwo {
+
+    public static void main(String[] args) {}
+
+}
