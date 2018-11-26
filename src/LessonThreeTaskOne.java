be7ecc742656467ea89dda1e512989b6674fb754
WIVSW <olegspostnikov@gmail.com>
2018-11-27 01:05:39 +0500

    Add more or less

--- src/LessonThreeTaskOne.java
+++ src/LessonThreeTaskOne.java
@@ -10,7 +10,8 @@ import java.util.Scanner;
 /**
  * Написать программу, которая загадывает случайное число от 0 до 9, и пользователю дается 3
  * попытки угадать это число. При каждой попытке компьютер должен сообщить больше ли
- * указанное пользователем число, чем загаданное, или меньше.
+ * указанное пользователем число, чем загаданное, или меньше. После победы или проигрыша
+ * выводится запрос – «Повторить игру еще раз? 1 – да / 0 – нет»(1 – повторить, 0 – нет).
  */
 public class LessonThreeTaskOne {
 
@@ -32,7 +33,7 @@ public class LessonThreeTaskOne {
     private static void run() {
         int number = random.nextInt(LIMIT_RANDOM);
         int attemptsLeft = MAX_ATTEMPTS_COUNT;
-        boolean isSucceded;
+        int guess;
 
         /** TODO: Раскомментировать для тестирования */
         //System.out.println("Ответ: " + number);
@@ -41,10 +42,19 @@ public class LessonThreeTaskOne {
         do {
             System.out.printf("Попыток: %d\n", attemptsLeft);
 
-            isSucceded = number == scanner.nextInt();
+            guess = scanner.nextInt();
+            String message;
 
-            System.out.print(isSucceded ? "Угадали!" : "Не угадали. ");
-        } while (!isSucceded && --attemptsLeft > 0);
+            if (guess > number) {
+                message = "Не угадали. Число меньше. ";
+            } else if (guess < number) {
+                message = "Не угадали. Число больше. ";
+            } else {
+                message = "Угадали!";
+            }
+
+            System.out.print(message);
+        } while (guess != number && --attemptsLeft > 0);
     }
 
 }
