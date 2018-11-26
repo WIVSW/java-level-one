6500b79e1f99084061454349d06f154d5691f279
WIVSW <olegspostnikov@gmail.com>
2018-11-27 01:13:00 +0500

    Add words array

--- src/LessonThreeTaskTwo.java
+++ src/LessonThreeTaskTwo.java
@@ -6,6 +6,10 @@
  */
 public class LessonThreeTaskTwo {
 
+    private static String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot",
+            "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea",
+            "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+
     public static void main(String[] args) {}
 
 }
