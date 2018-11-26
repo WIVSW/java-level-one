23cf3f683592501948c964b01a98ca2d9f52c5fa
WIVSW <olegspostnikov@gmail.com>
2018-11-27 01:20:00 +0500

    Add scanner

--- src/LessonThreeTaskTwo.java
+++ src/LessonThreeTaskTwo.java
@@ -1,14 +1,17 @@
-import java.util.Random;
-
 /**
  * Java. Level 1. Lesson 3. Task 1. Practice
  *
  * @author Oleg Postnikov
  * @version dated Nov 27, 2018
  */
+import java.util.Random;
+import java.util.Scanner;
+
 public class LessonThreeTaskTwo {
 
     private static Random random = new Random();
+
+    private static Scanner scanner = new Scanner(System.in);
 
     private static String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot",
             "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea",
