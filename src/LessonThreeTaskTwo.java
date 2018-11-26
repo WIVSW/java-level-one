ba680472bab9599a04c23e6af377f1407c2de3b6
WIVSW <olegspostnikov@gmail.com>
2018-11-27 01:18:29 +0500

    Add getRandomWord method

--- src/LessonThreeTaskTwo.java
+++ src/LessonThreeTaskTwo.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * Java. Level 1. Lesson 3. Task 1. Practice
  *
@@ -6,10 +8,19 @@
  */
 public class LessonThreeTaskTwo {
 
+    private static Random random = new Random();
+
     private static String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot",
             "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea",
             "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        String word = getRandomWord();
+        System.out.println(word);
+    }
+
+    private static String getRandomWord() {
+        return words[random.nextInt(words.length)];
+    }
 
 }
