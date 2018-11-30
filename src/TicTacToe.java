efb7b8f135d2bc1239191438ba75ac67f9b7c947
WIVSW <olegspostnikov@gmail.com>
2018-11-30 21:12:37 +0500

    Create class TicTacToe

---
+++ src/TicTacToe.java
@@ -0,0 +1,11 @@
+/**
+ * Java. Level 1. Lesson 4. Tic Tac Toe
+ *
+ * @author Oleg Postnikov
+ * @version dated Dec 1, 2018
+ */
+public class TicTacToe {
+
+    public static void main(String[] args) {}
+
+}
