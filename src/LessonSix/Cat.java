c3d66384de3e197b8d1dc2816a39dd61cacd9449
WIVSW <olegspostnikov@gmail.com>
2018-12-09 14:15:54 +0500

    Add interface IAnimal

--- src/LessonSix/Cat.java
+++ src/LessonSix/Cat.java
@@ -5,4 +5,16 @@ class Cat extends Animal {
     Cat() {
         super(200, 2, Double.NEGATIVE_INFINITY);
     }
+
+    public void run(double meters) {
+        doAction("run", meters, runLimit);
+    }
+
+    public void jump(double meters) {
+        doAction("jump", meters, jumpLimit);
+    }
+
+    public void swim(double meters) {
+        doAction("swim", meters, swimLimit);
+    }
 }
--- src/LessonSix/Dog.java
+++ src/LessonSix/Dog.java
@@ -5,4 +5,16 @@ class Dog extends Animal {
     Dog() {
         super(500, 0.5, 100);
     }
+
+    public void run(double meters) {
+        doAction("run", meters, runLimit);
+    }
+
+    public void jump(double meters) {
+        doAction("jump", meters, jumpLimit);
+    }
+
+    public void swim(double meters) {
+        doAction("swim", meters, swimLimit);
+    }
 }
