6906da9057119f6cbcd8a14bde974bd31e2f1d86
WIVSW <olegspostnikov@gmail.com>
2018-12-11 00:04:19 +0500

    Refactor cats eat method

--- src/LessonSeven/LessonSeven.java
+++ src/LessonSeven/LessonSeven.java
@@ -7,26 +7,34 @@
 package LessonSeven;
 
 class LessonSeven {
-
-    public static void main(String[] args) {
-        Plate plate = new Plate(100);
-        Cat[] cats = {
+    private static Plate plate = new Plate(100);
+    private static Cat[] cats = {
             new Cat("Барсик", 10),
             new Cat("Кузя", 20),
             new Cat("Кекс", 30),
             new Cat("Мурзик", 40),
             new Cat("Персик", 50)
-        };
+    };
 
+    public static void main(String[] args) {
+        feedAllCats();
+        plate.info();
+
+        plate.add(50);
+        plate.info();
+
+        feedAllCats();
+        plate.info();
+    }
+
+    private static void feedAllCats() {
         System.out.println("Коты:");
+
         for(Cat cat : cats) {
             cat.eat(plate);
             System.out.println("\t" + cat);
         }
 
         System.out.println();
-        plate.info();
-        plate.add(50);
-        plate.info();
     }
 }
